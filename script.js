9f39c3181a960680079ad99818180bdaa802b7fe
Amir Nesru <amirnesru054@gmail.com>
2026-07-03 18:38:06 +0300

    feat: implement task completion toggle, delete functionality, and improve error message handling

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
-let Add = document.getElementById("addBtn")
-let taskInput = document.getElementById("taskInput")
-let taskList = document.getElementById("taskList")
+let Add = document.getElementById("addBtn");
+let taskInput = document.getElementById("taskInput");
+let taskList = document.getElementById("taskList");
+let error = document.getElementById("errorMsg");
 
-Add.addEventListener("click",function() {
-    if (taskInput.value.trim() === ""){
-        alert("Please type a task first") ;
-    }
+
+Add.addEventListener("click", function () {
+    if (taskInput.value.trim() === "") {
+        error.textContent = "Please type a task first";
+    } 
     else {
-        let li = document.createElement("li") ;
-        li.innerHTML = taskInput.value ;
-        taskList.appendChild(li) ;
+        error.textContent = "";
+
+        let li = document.createElement("li");
+
+        let span = document.createElement("span");
+        span.textContent = taskInput.value;
+        span.classList.add("task-text");
+
+        let btn = document.createElement("button");
+        btn.textContent = "Done";
+        btn.classList.add("done-btn");
+
+        let deleteBtn = document.createElement("button")
+        deleteBtn.textContent = "Delete"
+        deleteBtn.classList.add("delete-btn");
+
+        li.classList.add("task-item");
+
+        li.appendChild(span);
+        li.appendChild(btn);
+        li.appendChild(deleteBtn)
+
+        taskList.appendChild(li);
+
+        btn.addEventListener ("click", function() {
+            li.classList.toggle("done");
+        })
+
+        deleteBtn.addEventListener ("click", function (){
+            li.remove();
+            
+        })
     }
-    taskInput.value = "" ;
-})
 
-
+    taskInput.value = "";
+});
